5bb7434697b87c9addbe18ac66b4f010e5dc3076
JibbaJamba <nieto_b@yahoo.com>
2019-09-30 17:51:26 -0700

    Migrated project to GitHub as the primary location.  Updated NuGet packages to accomodate some of the latest and greatest out of NuGet.

--- Templates.Framework/Properties/AssemblyInfo.cs
+++ Templates.Framework/Properties/AssemblyInfo.cs
@@ -4,10 +4,9 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Templates.Framework")]
 [assembly: AssemblyDescription("Provides interfaces to contructs that aid in T4 Templating.")]
-[assembly: AssemblyCompany("Hyster Yale Group")]
 [assembly: AssemblyProduct("Templates.Framework")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("2.0.*")]
+[assembly: AssemblyVersion("2.1.*")]
--- Templates/Properties/AssemblyInfo.cs
+++ Templates/Properties/AssemblyInfo.cs
@@ -4,9 +4,8 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Templates")]
 [assembly: AssemblyDescription("Provides contructs that aid in T4 Templating.")]
-[assembly: AssemblyCompany("Hyster Yale Group")]
 [assembly: AssemblyProduct("Templates")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
 
 [assembly: ComVisible(false)]
 
